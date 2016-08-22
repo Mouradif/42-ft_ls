c0861a5f7b335a3f1751d25f8b70d9b4436f6818
Mourad Kejji <mkejji@gmail.com>
2016-08-22 06:56:58 +0200

    lst and tree

--- ft_ls.h
+++ ft_ls.h
@@ -1,7 +1,15 @@
-typedef struct	s_entry
+typedef struct	s_elt
 {
-	char	*filename;
-	size_t	ts_edit;
-	size_t	ts_create;
-	size_t	ts_access;
-}				t_entry;
+	char			*filename;
+	size_t			ts_edit;
+	size_t			ts_create;
+	size_t			ts_access;
+	struct s_elt	*next;
+}				t_elt;
+
+typedef struct	s_etree
+{
+	t_elt 			*elt;
+	struct s_etree	*left;
+	struct s_etree	*right;
+}				t_etree;
